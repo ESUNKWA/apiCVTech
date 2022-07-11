465c7bf7ff92283aa956a9ac0a7be5c896a7eb74
Esunkwa <kouadiodeki@gmail.com>
2022-07-11 09:40:55 +0000

    Création d'un nouveau controlleur pour la connexion et la déconnexion

--- app/Http/Controllers/user/loginController.php
+++ app/Http/Controllers/user/loginController.php
@@ -2,12 +2,18 @@
 
 namespace App\Http\Controllers\user;
 
-use App\Http\Controllers\Controller;
 use App\Models\rc;
+use App\Models\User;
 use Illuminate\Http\Request;
+use App\Http\traits\responseTrait;
+use App\Http\Controllers\Controller;
+use Illuminate\Support\Facades\Auth;
+use Illuminate\Support\Facades\Hash;
+use Illuminate\Support\Facades\Validator;
 
 class loginController extends Controller
 {
+    use responseTrait;
     /**
      * Display a listing of the resource.
      *
@@ -82,5 +88,57 @@ class loginController extends Controller
     public function destroy(rc $rc)
     {
         //
+    }
+
+    public function login(Request $request){
+
+        $inputs = $request->all();
+
+        //Validation des données
+
+        $errors = [
+            'email' => 'required',
+            'password' => 'required',
+        ];
+
+        $erreurs = [
+            'email.required' => 'L\'addresse email est réquis',
+            'password.required' => 'Le mot de passe est réquis',
+        ];
+
+        $validation = Validator::make($inputs, $errors, $erreurs);
+
+        if( !$validation->fails() ){
+
+            try {
+
+                //Enregistrement des données dans la base de données
+
+                $utilisateur = User::where('email', '=', $request->email)->first();
+
+                if(Hash::check($request->password, $utilisateur->password)){
+
+                    // création du token;
+                    $token = $utilisateur->createToken('auth_token')->plainTextToken;
+
+                    return $this->responseSuccess('Utilisateur trouvé', [json_decode($utilisateur)], $token);
+
+                }else{
+                    return $this->responseError('');
+                }
+
+            } catch (\Exception $th) {
+                return $this->responseError($th->getMessage());
+            }
+
+        }else{
+            return $validation->errors();
+        }
+
+    }
+
+    public function logout() {
+        Auth::user()->tokens()->delete();
+        return $this->responseSuccess('Vous êtes maintenant déconnecté');
     }
 }
--- routes/api.php
+++ routes/api.php
@@ -2,16 +2,17 @@
 
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\user\loginController;
 use App\Http\Controllers\cv\UtilusateursController;
 
 Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
     return $request->user();
 });
-Route::post('utilisateur.login', [UtilusateursController::class,'login']);
+Route::post('login', [loginController::class,'login']);
 Route::group([ 'middleware' => 'auth:sanctum'], function(){
     //Route relative au controller UtilusateursController
     Route::resource('utilisateur', 'App\Http\Controllers\cv\UtilusateursController');
-    Route::post('utilisateur.logout', [UtilusateursController::class,'logout']);
+    Route::post('logout', [loginController::class,'logout']);
 
     //Route relative au controller ExperienceController
     Route::resource('experience', 'App\Http\Controllers\cv\ExperiencesProController');
